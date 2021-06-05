89437d896ef5e35fef93443db691f9bc89cd4853
Raihan Tajdid <raihantajdid@gmail.com>
2021-06-06 04:43:11 +0600

    advance js practice

---
+++ true-false.js
@@ -0,0 +1,23 @@
+
+// Truthy value
+// any number(positive or negative), " " , "0", true, " "
+
+// falsy value
+// 0, "", NaN, undefind, null, 
+const rollNumber = -10;
+if(rollNumber){
+    console.log("Condition True.");
+}
+else{
+    console.log("Condition false.")
+}
+
+// for string
+const studentName = '';
+if(studentName){
+    console.log("Condition True.");
+}
+else{
+    console.log("Condition false.")
+}
+// in that case zero, white space in a ""/'' would be true.
